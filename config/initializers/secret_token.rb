bb3c65d150fb64ddd1baa04fa999022e97ce79c6
Ricardo Rosales <missingcharacter@gmail.com>
2012-05-05 10:01:26 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Congresoplon::Application.config.secret_token = 'df462fdce35973ffb4dad16865cf1d1638543048d7f80bb317fe39a9d99ec6feaf187a49645af3fe797509e57801ef7f5fae58678df3b72ffa90296184fa4dd3'
